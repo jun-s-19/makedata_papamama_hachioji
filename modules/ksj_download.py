a4373b5d60a1a4f3a0506c0b0af9f0b9a0013dee
jun-s-19 <jun.sugawa@gmail.com>
2019-06-29 23:12:30 +0900

    関数のdocstringを追記

--- modules/ksj_download.py
+++ modules/ksj_download.py
@@ -1,3 +1,7 @@
+"""
+国土数値情報データダウンロードサービスからデータをダウンロードする際に
+利用する関数群を定義
+"""
 
 # モジュールインポート
 import requests
@@ -5,16 +9,43 @@ import xml.etree.ElementTree as ET
 from pathlib import Path
 
 
-# 最新のzipファイルダウンロードURLを取得する
 def get_ksj_zipurl_latest(url_base, params_dict):
+    """最新のzipファイルダウンロードURLを取得する
+    
+    Parameters
+    ----------
+    url_base : [url]
+        Web APIのベースとなるURL
+    params_dict : dict
+        クエリのパラメータ(dict)の辞書
+    
+    Returns
+    -------
+    [url]
+        最新のzipファイルダウンロードURL
+    """
     zipurl_dict={}
     for elem in params_dict.keys():
         zipurl_dict[elem] = get_ksj_zipurl(url_base,params_dict[elem])[-1]
     
     return zipurl_dict
 
-# zipファイルダウンロード用のURLを取得する
+# 
 def get_ksj_zipurl(url_base, params):
+    """zipファイルダウンロード用のURLを取得する
+    
+    Parameters
+    ----------
+    url_base : [url]
+        Web APIのベースとなるURL
+    params : [dict]
+        クエリのパラメータ
+    
+    Returns
+    -------
+    [list]
+        'zipFileUrl'に対応する値のリスト
+    """
     response = requests.get(url_base, params)
     xml_string = response.text
     root = ET.fromstring(xml_string)
@@ -26,8 +57,17 @@ def get_ksj_zipurl(url_base, params):
     return zipurl_list
 
 
-# 指定したURLからファイルをダウンロードする
+# 
 def download(url, save_directory_path):
+    """指定したURLからファイルをダウンロードする
+    
+    Parameters
+    ----------
+    url : [str]
+        ダウンロード対象のURL
+    save_directory_path : [str]
+        ダウンロードしたファイルを格納するディレクトリのパス名
+    """
     file_name = Path(url).name
     p_dir = Path(save_directory_path)
 
@@ -36,3 +76,5 @@ def download(url, save_directory_path):
         with open(p_dir / file_name, 'wb') as file:
             for chunk in res.iter_content(chunk_size=1024):
                 file.write(chunk)
+    else:
+        print("Download Error. Response status:", res.status_code)
